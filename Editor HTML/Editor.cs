6f89cb6a3f31e855b1f13fe422b947fc75dad292
isahonorato <isabelladdotcom@gmail.com>
2023-04-19 22:47:50 -0300

    Editor HTML

---
+++ Editor HTML/Editor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Editor_HTML
+{
+    public static class Editor
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO EDITOR");
+            Console.WriteLine("----------------");
+            Start();
+        }
+        public static void Start()
+        {
+            var file = new StringBuilder();
+            do
+            {
+                file.Append(Console.ReadLine());
+                file.Append(Environment.NewLine);
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Deseja salvar o arquivo?");
+        }
+    }
+}
